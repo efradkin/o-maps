79a294ce3f89f9bfbfeba850e145e9cb25c0075b
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2024-11-11 14:02:29 +0300

    Диаграмма статистики по годам издания карт.

--- js/charts.js
+++ js/charts.js
@@ -15,6 +15,7 @@ window.onload = function buildCharts() {
 
     buildChart(
         document.getElementById('mapTypeChart'),
+        mapTypes,
         mapsStat,
         'Количество карт');
     document.getElementById('totalMaps').innerHTML = oMaps.length;
@@ -31,9 +32,16 @@ window.onload = function buildCharts() {
 
     buildChart(
         document.getElementById('mapAreaChart'),
+        mapTypes,
         mapAreas,
         'Площадь карт (км²)');
     document.getElementById('totalArea').innerHTML = mapAreas.reduce((a, c) => a + c).toFixed(0);
+
+    buildChart(
+        document.getElementById('mapYearChart'),
+        ['2020-е', '2010-е', '2000-е', '1990-е', 'Ретро', '???'],
+        calcYears(),
+        'Год издания');
 }
 
 function calcMapsArea(group) {
@@ -45,11 +53,38 @@ function calcMapsArea(group) {
     return area;
 }
 
-function buildChart(ctx, data, label) {
+function calcYears() {
+    let y2020 = 0, y2010 = 0, y2000 = 0, y1990 = 0, yRetro = 0, yUnknown = 0;
+    for (const m of oMaps) {
+        if (m.year) {
+            if (m.year < 1990) {
+                yRetro++;
+            } else
+            if (m.year < 2000) {
+                y1990++;
+            } else
+            if (m.year < 2010) {
+                y2000++;
+            } else
+            if (m.year < 2020) {
+                y2010++;
+            } else {
+                y2020++;
+            }
+        } else {
+            yUnknown++;
+        }
+    }
+    return [
+        y2020, y2010, y2000, y1990, yRetro, yUnknown
+    ]
+}
+
+function buildChart(ctx, labels, data, label) {
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: mapTypes,
+            labels: labels,
             datasets: [{
                 label: label,
                 data: data,
@@ -64,7 +99,7 @@ function buildChart(ctx, data, label) {
                     ticks: {
                         color: "black",
                         font: {
-                            size: 14,
+                            size: 12,
                         },
                     }
                 },
@@ -72,7 +107,7 @@ function buildChart(ctx, data, label) {
                     ticks: {
                         color: "black",
                         font: {
-                            size: 14,
+                            size: 12,
                         },
                         // stepSize: 1,
                         beginAtZero: true
@@ -84,7 +119,7 @@ function buildChart(ctx, data, label) {
                     labels: {
                         color: "black",
                         font: {
-                            size: 28
+                            size: 14
                         }
                     }
                 }
