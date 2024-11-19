0fc85eff7b8a0936531ae41eaf418c4911e7b34e
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2024-11-20 00:57:38 +0300

    maxZindex processing is fixed

--- js/main.js
+++ js/main.js
@@ -438,7 +438,12 @@ function onMapSelect(ovrl, map) {
     selectedOverlay = ovrl;
     selectedMap = map;
 
-    ovrl.getElement().style.zIndex = maxZindex++;
+    maxZindex++;
+    let zIndex = ovrl.getElement().style.zIndex;
+    if (zIndex > maxZindex) {
+        maxZindex = zIndex + 1;
+    }
+    ovrl.getElement().style.zIndex = maxZindex;
 
     if (editMode) {
         marker1.setLatLng(ovrl.getTopLeft());
