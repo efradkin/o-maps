308e38afe1b60e5423358aac5037081fd5f25526
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2025-01-16 11:30:30 +0300

    Оптимизировал панель выбора слоёв в Москве

---
+++ js/map-build-moscow.js
@@ -0,0 +1,16 @@
+function buildOverlayMapsContents() {
+    return {
+        "Город": cityGroup,
+        "Парки": parkGroup,
+        "Лес": forestGroup,
+        "Специальные": specialGroup,
+        "Все": groupAllOrient,
+        "<span class='layer-separator'>2020-е</span>": group2020th,
+        "2010-е": group2010th,
+        "2000-е": group2000th,
+        "90-е": group90th,
+        "Ретро": groupRetro,
+        "???": groupUnknownYear,
+        "<span class='layer-separator'>Рогейн</span>": rogaineGroup,
+    };
+}
