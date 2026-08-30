66f6c06501ab55bb41fdd84f73008e372aa5f2e9
Evgeny Fradkin <Eugene.Fradkin@gmail.com>
2026-08-30 16:09:40 +0300

    Обновил кату Тихого Отдыха от Шириняна.

--- js/maps-parks.js
+++ js/maps-parks.js
@@ -1760,7 +1760,7 @@ let parkMaps = [
         year: 2026,
         url: './maps/parks/tikhy_otdyh_2026.webp',
         link: ['./original_maps/parks/tikhy_otdyh_2026_omaps.webp','./original_maps/parks/tikhy_otdyh_2026_pictures.pdf'],
-        bounds: [[59.9814393, 30.2850795], [59.9798075, 30.2999711], [59.9771558, 30.2833843]],
+        bounds: [[59.9813963, 30.2852941], [59.9797646, 30.3009152], [59.9769518, 30.2835345]],
         owner: 'SHRNN',
         author: 'SHRNN',
         editor: 'MSLVA',
